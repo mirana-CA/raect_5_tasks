da097c4825189d3cf50d71d96bbc8bd8dd4ff81d
mirana-CA <miranafm-bp216@code.edu.az>
2024-12-05 14:02:49 +0400

    5 tasks added

--- src/pages/collapse/index.jsx
+++ src/pages/collapse/index.jsx
@@ -2,7 +2,33 @@ import React, { useState } from "react";
 import "./index.scss";
 
 const Collapse = () => {
-  return <></>;
+  const [collpaseVisible, setCollpaseVisible] = useState(false);
+  return (
+    <div className="collapse">
+      <div className="collapse_elem">
+        <div
+          className="collapse_heading"
+          onClick={() => setCollpaseVisible(!collpaseVisible)}
+        >
+          Hello
+        </div>
+
+        <div
+          className={`${
+            collpaseVisible ? "collapse_text" : "collapse_text text_unvisible "
+          }`}
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, rem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, rem.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Collapse;
+// text_unvisible
